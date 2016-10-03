c1cbd7c949075944a212b649b87f83f809550ef8
pwentz <patrick.w.wentz@gmail.com>
2016-10-03 16:01:46 -0600

    close #8 add styling and css, also now clearing inputs on submit.

--- app/assets/javascripts/newIdeasContainer.js
+++ app/assets/javascripts/newIdeasContainer.js
@@ -1,15 +1,25 @@
 class NewIdeasContainer {
   renderInputs() {
     $('.app').prepend(
-      `<input type='text' class='new-title' />
-       <br>
-       <input type='text' class='new-body' />
-       <br>
-       <button
-         type='submit'
-         class='primary button add-idea'>
-         Submit
-       </button>`
+      `<div class='filler'></div>
+      <h1 id='header'>Idea box</h1>
+      <div class='row'>
+       <div class='small-6 columns small-centered'>
+        <input type='text' class='new-title' />
+        <br>
+        <input type='text' class='new-body' />
+        <br>
+        <div class='row'>
+          <div class='small-2 columns small-centered'>
+            <button
+              type='submit'
+              class='primary button hollow add-idea'>
+              Submit
+            </button>
+          </div>
+        </div>
+       </div>
+      </div>`
     )
   }
 }
