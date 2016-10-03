6e5b3e8641cb5f5fce79bd4beeaf3461ab448ff8
pwentz <patrick.w.wentz@gmail.com>
2016-10-03 07:39:46 -0600

    close #3 update individual ideas on blur.

--- app/assets/javascripts/ideasContainer.js
+++ app/assets/javascripts/ideasContainer.js
@@ -9,8 +9,18 @@ class IdeasContainer {
   formattedIdeas(ideas) {
     return ideas.map(i => {
       return `<div id=${i.id}>
-                <h1>${i.title}</h1>
-                <h5>${i.body}</h5>
+                <h1
+                  class='idea-title'
+                  contentEditable='true'
+                >
+                  ${i.title}
+                </h1>
+                <h5
+                  class='idea-body'
+                  contentEditable='true'
+                >
+                  ${i.body}
+                </h5>
                 <p>${i.quality}</p>
                 <button class='button alert hollow delete-idea'>
                   <i class='fi-x'></i>
@@ -28,11 +38,41 @@ class IdeasContainer {
   }
 
   handleDelete() {
-    idea = event.target.closest('div')
-    api.ideasDelete(idea.id)
+    api.ideasDelete(this.targetIdea().id)
       .done(data => {
         $('.ideas div').hide()
         this.renderIdeas()
       })
+  }
+
+  handleUpdate() {
+    this.stashIdea()
+    api.ideasUpdate(this.targetIdea().id)
+      .done(data => {
+        $('.ideas div').hide()
+        this.renderIdeas()
+      })
+  }
+
+  stashIdea() {
+    localStorage.setItem(
+      this.targetIdea().id,
+      JSON.stringify(
+        { title: this.targetTitle(),
+          body: this.targetBody() }
+      )
+    )
+  }
+
+  targetIdea() {
+    return event.target.closest('div')
+  }
+
+  targetTitle() {
+    return $(`#${this.targetIdea().id} .idea-title`)[0].innerText
+  }
+
+  targetBody() {
+    return $(`#${this.targetIdea().id} .idea-body`)[0].innerText
   }
 }
